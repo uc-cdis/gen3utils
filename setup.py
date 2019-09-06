9af1e7336e63b8754f55d3a6ed4f3f1344e4d601
Di Wu <di@Dis-MacBook-Pro.local.cdis>
2019-09-06 15:24:51 -0500

    Cut from gh:uc-cdis/template-repo

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+This is a file to describe the Python module distribution and
+helps with installation.
+
+More info on various arguments here:
+https://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
+from setuptools import setup, find_packages
+from subprocess import check_output
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode('utf-8').strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
+
+setup(
+    name="gen3utils",
+    version=get_version(),
+    description="Gen3 Library Template",
+    url="https://github.com/uc-cdis/gen3utils",
+    license="Apache",
+    packages=find_packages(),
+    install_requires=[
+        "cdiserrors~=0.1",
+    ],
+)
