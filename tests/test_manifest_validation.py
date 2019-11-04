baa3e4f9fd9297aa364c7f2429fba0b451c3592f
Di Wu <di@Dis-MacBook-Pro.local.cdis>
2019-11-04 15:15:24 -0600

    chore(format)

--- tests/test_manifest_validation.py
+++ tests/test_manifest_validation.py
@@ -52,94 +52,76 @@ def test_versions_validation_needs(validation_config):
     assert not ok, "fence 4.4.4 + arborist 1.0.0 should not pass validation"
 
     versions_block = {
-       "sower": "quay.io/cdis/sower:0.3.0",
-       "guppy": "quay.io/cdis/guppy:0.3.0",
+        "sower": "quay.io/cdis/sower:0.3.0",
+        "guppy": "quay.io/cdis/guppy:0.3.0",
     }
     ok = versions_validation(versions_block, validation_config["versions"])
     assert ok, "sower + guppy should not pass validation"
 
-    versions_block = {
-       "sower": "quay.io/cdis/sower:0.3.0",
-    }
+    versions_block = {"sower": "quay.io/cdis/sower:0.3.0"}
     ok = versions_validation(versions_block, validation_config["versions"])
     assert not ok, "sower without guppy should not pass validation"
+
 
 def test_blocks_validation(validation_config):
     """
     Test validation of sevice having block requirements in manifest for validation
     """
     block_requirement = {
-        "versions": {
-          "arborist": "quay.io/cdis/arborist:2.2.0"
-        },
-        "arborist": {
-          "deployment_version": "2"
-        }        
+        "versions": {"arborist": "quay.io/cdis/arborist:2.2.0"},
+        "arborist": {"deployment_version": "2"},
     }
     ok = blocks_validation(block_requirement, validation_config["block"])
-    assert ok, "arborist 2.2.0 with deployment_version in arborist blcok should pass validation"
+    assert (
+        ok
+    ), "arborist 2.2.0 with deployment_version in arborist blcok should pass validation"
 
-    block_requirement = {
-        "versions": {
-          "arborist": "quay.io/cdis/arborist:2.2.0"
-        }   
-    }
+    block_requirement = {"versions": {"arborist": "quay.io/cdis/arborist:2.2.0"}}
     ok = blocks_validation(block_requirement, validation_config["block"])
-    assert not ok, "arborist 2.2.0 without deployment_version in arborist blcok should not pass validation"
+    assert (
+        not ok
+    ), "arborist 2.2.0 without deployment_version in arborist blcok should not pass validation"
 
     block_requirement = {
-        "versions": {
-          "hatchery": "quay.io/cdis/hatchery:0.1.0"
-        },
+        "versions": {"hatchery": "quay.io/cdis/hatchery:0.1.0"},
         "hatchery": {
-          "sidecar": {
-              "cpu-limit": "1.0",
-              "memory-limit": "256Mi",
-              "image": "quay.io/cdis/gen3fuse-sidecar:chore_sidecar",
-              "env": {"NAMESPACE":"default", "HOSTNAME": "whatever"},
-              "args": [],
-              "command": ["/bin/bash", "/sidecarDockerrun.sh"],
-              "lifecycle-pre-stop": ["su", "-c", "echo test", "-s", "/bin/sh", "root"]
-          }
-        }        
+            "sidecar": {
+                "cpu-limit": "1.0",
+                "memory-limit": "256Mi",
+                "image": "quay.io/cdis/gen3fuse-sidecar:chore_sidecar",
+                "env": {"NAMESPACE": "default", "HOSTNAME": "whatever"},
+                "args": [],
+                "command": ["/bin/bash", "/sidecarDockerrun.sh"],
+                "lifecycle-pre-stop": [
+                    "su",
+                    "-c",
+                    "echo test",
+                    "-s",
+                    "/bin/sh",
+                    "root",
+                ],
+            }
+        },
     }
     ok = blocks_validation(block_requirement, validation_config["block"])
     assert ok, "hatchery with sidecar in hatchery blcok should pass validation"
 
-    block_requirement = {
-        "versions": {
-          "hatchery": "quay.io/cdis/hatchery:0.1.0"
-        }
-    }
+    block_requirement = {"versions": {"hatchery": "quay.io/cdis/hatchery:0.1.0"}}
     ok = blocks_validation(block_requirement, validation_config["block"])
-    assert not ok, "hatchery without sidecar in hatchery blcok should not pass validation"
+    assert (
+        not ok
+    ), "hatchery without sidecar in hatchery blcok should not pass validation"
 
     block_requirement = {
-        "versions": {
-          "guppy": "quay.io/cdis/guppy:0.3.0"
-        },
+        "versions": {"guppy": "quay.io/cdis/guppy:0.3.0"},
         "guppy": {
-            "indices": [
-              {
-                "index": "test",
-                "type": "case"
-              }
-            ],
-
-            "auth_filter_field": "auth_resource_path"
-          }     
+            "indices": [{"index": "test", "type": "case"}],
+            "auth_filter_field": "auth_resource_path",
+        },
     }
     ok = blocks_validation(block_requirement, validation_config["block"])
     assert ok, "guppy with guppy block should pass validation"
 
-    block_requirement = {
-        "versions": {
-          "guppy": "quay.io/cdis/guppy:0.3.0"
-        }   
-    }
+    block_requirement = {"versions": {"guppy": "quay.io/cdis/guppy:0.3.0"}}
     ok = blocks_validation(block_requirement, validation_config["block"])
     assert not ok, "guppy without guppy block should pass validation"
-
-
-
-
