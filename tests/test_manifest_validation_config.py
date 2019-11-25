1d88238cd8b70278dcdc149aa4a54c593e977e31
Pauline <ribeyre@uchicago.edu>
2019-11-24 22:31:25 -0600

    fix bug

---
+++ tests/test_manifest_validation_config.py
@@ -0,0 +1,13 @@
+def test_min_max(validation_config):
+    message = "For now, we do not handle versions validation with either min or max but not both - either fix the config or update the validation code to handle it: {}"
+    for requirement in validation_config.get("versions"):
+        for key, details in requirement.items():
+            if key != "needs":
+                assert ("min" in details and "max" in details) or (
+                    "min" not in details and "max" not in details
+                ), message.format(requirement)
+            else:
+                for need_details in details.values():
+                    assert ("min" in need_details and "max" in need_details) or (
+                        "min" not in need_details and "max" not in need_details
+                    ), message.format(requirement)
