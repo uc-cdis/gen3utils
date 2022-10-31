d545cdf3adfda8a5962ee32760700964cdda072b
Pauline <4224001+paulineribeyre@users.noreply.github.com>
2022-10-31 15:55:43 -0500

    simplify etl mapping unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,39 +26,9 @@ def etl_mapping_validation_dict():
 
 
 @pytest.fixture(scope="session")
-def etl_mapping_validation_mapping():
-    return "tests/data/etlMapping.yaml"
-
-
-@pytest.fixture(scope="session")
 def etl_mapping_validation_manifest():
     with open("tests/data/manifest.json") as f:
         return json.load(f)
-
-
-@pytest.fixture(scope="session")
-def etl_mapping_validation_mapping_failed():
-    return "tests/data/etlMapping_constraints_error.yaml"
-
-
-@pytest.fixture(scope="session")
-def etl_mapping_validation_duplication_failed():
-    return "tests/data/etlMapping_duplicate_field_error.yaml"
-
-
-@pytest.fixture(scope="session")
-def etl_mapping_validation_format_failed():
-    return "tests/data/etlMapping_format_error.yaml"
-
-
-@pytest.fixture(scope="session")
-def etl_mapping_validation_mapping_collector():
-    return "tests/data/etlMapping_collector.yaml"
-
-
-@pytest.fixture(scope="session")
-def etl_mapping_validation_mapping_collector_unknown_prop():
-    return "tests/data/etlMapping_collector_unknown_prop.yaml"
 
 
 @pytest.fixture(scope="function")
